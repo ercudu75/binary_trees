53e3d1a24c3adb3c24da561131c7722f9df00913
ercudu <ilyass.bentalha2@gmail.com>
2024-02-27 18:24:11 +0000

    add task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ *
+ * This function deletes an entire binary tree recursively.
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+}
