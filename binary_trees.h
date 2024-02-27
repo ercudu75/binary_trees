6c57d1dac2736f802f10a02bef4ffd7ac067b15f
ercudu <ilyass.bentalha2@gmail.com>
2024-02-27 18:47:33 +0000

    travesral in binary trees(task 6-7-8)

--- binary_trees.h
+++ binary_trees.h
@@ -39,6 +39,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 
 
 #endif
