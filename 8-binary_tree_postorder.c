13a53224b0cbf74032d05b372aca3b7a75020281
ercudu <ilyass.bentalha2@gmail.com>
2024-02-27 18:57:21 +0000

    idk what requirement failed the task

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -4,6 +4,7 @@
  * binary_tree_postorder - Goes through a binary tree using post-order
  * @tree: Pointer to the root node of the tree to traverse
  * @func: Pointer to a function to call for each node
+ *
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
