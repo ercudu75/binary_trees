ee25af908449afae4cdfb5e57f28d5937f85bd3d
thevorgx <kh.elallali@gmail.com>
2024-02-27 21:13:35 +0100

    test new files

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,24 +2,23 @@
 
 
 /**
- * binary_tree_node - Creates a new binary tree node
- * @parent: Pointer to the parent node of the new node
- * @value: Value to store in the new node
- *
- * Return: Pointer to the newly created node, or NULL on failure
+ * binary_tree_node - Entry point
+ * @parent: the parent node or root node
+ * @value: value the node will take
+ * Return: Always node (Success) other ways return NULL
  */
+
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *node;
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (!new_node)
+	node = malloc(sizeof(binary_tree_t));
+	if (!node)
 		return (NULL);
-
-	new_node->parent = parent;
-	new_node->n = value;
-	new_node->left = NULL;
-	new_node->right = NULL;
-
-	return (new_node);
+	node->n = value;
+	node->left = NULL;
+	node->right = NULL;
+	node->parent = parent;
+	return (node);
 }
--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,16 +1,31 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - goes through a tree using in-order traversal.
- * @tree: pointer to the root node of the tree to traverse.
- * @func: pointer to a function to call for each node.
+ * in_order - function helper in_order traversal
+ * using recursion
+ * @root: the node of the tree
+ * @func: pointer to function that take integer as argument
  */
+
+void in_order(const binary_tree_t *root, void (*func)(int))
+{
+	if (!root)
+		return;
+	in_order(root->left, func);
+	func(root->n);
+	in_order(root->right, func);
+}
+
+/**
+ * binary_tree_inorder - function that goes through a
+ * binary tree using  in-order traversal
+ * @tree: the tree it self
+ * @func: pointer to function that take integer as argument
+ */
+
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
-
-	binary_tree_inorder(tree->left, func);
-	func(tree->n);
-	binary_tree_inorder(tree->right, func);
+	in_order(tree, func);
 }
