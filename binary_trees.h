8cd59314d9a3452f81e7a825b5ef33a207b265e2
ercudu <ilyass.bentalha2@gmail.com>
2024-02-27 16:07:47 +0000

    add task 0

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,8 @@
 #define BINARY_TREES
 
 
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -42,5 +44,7 @@ typedef struct binary_tree_s heap_t;
 */
 void binary_tree_print(const binary_tree_t *);
 
+/* fucntions */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif
