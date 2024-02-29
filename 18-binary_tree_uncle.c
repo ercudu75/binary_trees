4a0aea24f5f8c6e223d5baf3df51646083ad7be5
thevorgx <kh.elallali@gmail.com>
2024-02-29 03:18:12 +0100

    update

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -13,16 +13,15 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	binary_tree_t *right_uncle;
 	binary_tree_t *grand_father;
 
-	grand_father = node->parent->parent;
-	if (node == NULL || node->parent == NULL || grand_father == NULL)
+	if (node == NULL || node->parent->parent == NULL)
 		return (NULL);
-
+	grand_father = node->parent->parent;
 	left_uncle = grand_father->left;
 	right_uncle = grand_father->right;
-	if (node->parent == left_uncle && right_uncle != NULL)
+	if (node->parent == left_uncle)
 		return (right_uncle);
 
-	if (node->parent == right_uncle && left_uncle != NULL)
+	if (node->parent == right_uncle)
 		return (left_uncle);
 
 	return (NULL);
